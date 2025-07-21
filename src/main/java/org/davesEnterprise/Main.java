129e2842354497c04623860a9d17d15229744c28
Dave <80040393+DaveWasTakn@users.noreply.github.com>
2025-07-21 18:59:04 +0200

    cleanup

--- src/main/java/org/davesEnterprise/Main.java
+++ src/main/java/org/davesEnterprise/Main.java
@@ -6,8 +6,11 @@ import org.davesEnterprise.download.DownloaderBuilder;
 public class Main {
     public static void main(String[] args) {
 
+        // TODO proper argument handling and stuff
+        // TODO GUI :-)
+
         Downloader downloader = new DownloaderBuilder()
-                .setRetries(50)
+                .setRetries(100)
                 .setPlaylist(args[0])
                 .create();
 
