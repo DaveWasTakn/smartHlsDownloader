5068774c0c502758af97ce010e242c68eb220fa0
Dave <80040393+DaveWasTakn@users.noreply.github.com>
2025-07-29 00:00:13 +0200

    design swing GUI

---
+++ src/main/java/org/davesEnterprise/GuiForm.java
@@ -0,0 +1,17 @@
+package org.davesEnterprise;
+
+import javax.swing.*;
+
+public class GuiForm extends JPanel {
+    public JPanel mainPanel;
+
+    private JSpinner validations;
+    private JSpinner retries;
+    private JTextArea uri;
+    private JTextField output;
+    private JProgressBar progressDownload;
+    private JProgressBar progressValidation;
+    private JButton start;
+    private JSpinner downloads;
+    private JComboBox validation;
+}
--- src/main/java/org/davesEnterprise/Main.java
+++ src/main/java/org/davesEnterprise/Main.java
@@ -1,27 +1,51 @@
 package org.davesEnterprise;
 
+import com.formdev.flatlaf.intellijthemes.FlatDarkPurpleIJTheme;
 import org.davesEnterprise.download.Downloader;
 import org.davesEnterprise.download.DownloaderBuilder;
 import org.davesEnterprise.util.Args;
+
+import javax.swing.*;
+import java.awt.*;
 
 public class Main {
     public static void main(String[] args) {
         // TODO GUI :-)
         // TODO adaptiveness ? ie lower quality segments
 
-        Args.init(args);
-        Args arguments = Args.get();
+        if (args.length > 0) {
+            Args.init(args);
+            Args arguments = Args.get();
 
-        Downloader downloader = new DownloaderBuilder(arguments.output)
-                .setRetries(arguments.retries)
-                .setPlaylist(arguments.playlistUrl)
-                .setConcurrentDownloads(arguments.concurrentDownloads)
-                .setConcurrentValidations(arguments.concurrentValidations)
-                .setResume(arguments.resume) // TODO implement resume functionality ?
-                .setSegmentValidation(arguments.segmentValidation)
-                .build();
+            Downloader downloader = new DownloaderBuilder(arguments.output)
+                    .setRetries(arguments.retries)
+                    .setPlaylist(arguments.playlistUrl)
+                    .setConcurrentDownloads(arguments.concurrentDownloads)
+                    .setConcurrentValidations(arguments.concurrentValidations)
+                    .setResume(arguments.resume) // TODO implement resume functionality ?
+                    .setSegmentValidation(arguments.segmentValidation)
+                    .build();
 
-        downloader.start();
+            downloader.start();
+            System.exit(0);
+        }
+
+        try {
+            UIManager.setLookAndFeel(new FlatDarkPurpleIJTheme());
+        } catch (Exception ex) {
+            System.err.println("Failed to initialize LaF");
+        }
+
+        SwingUtilities.invokeLater(() -> {
+            JFrame frame = new JFrame("GuiForm");
+            frame.setMinimumSize(new Dimension(0, 0));
+            GuiForm guiForm = new GuiForm();
+            guiForm.setMinimumSize(new Dimension(0, 0));
+            frame.setContentPane(guiForm.mainPanel);
+            frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+            frame.pack();
+            frame.setVisible(true);
+        });
 
     }
 
